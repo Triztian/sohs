dd16332df136a501d737ffe67f088f3874052948
Tristian Azuara <tristian.azuara@gmail.com>
2018-02-25 19:10:30 -0800

    Add list section

--- 0.basics.hs
+++ 0.basics.hs
@@ -19,8 +19,29 @@ foo n
   | n `mod` 17 == 2 = -43
   | otherwise       = n + 3
 
+-- Pairs
+p :: (Int, Char)
+p = (3, 'x')
+
+sumPair :: (Int, Int) -> Int
+sumPair (x, y) = x + y
+
+
+-- Constructing lists
+emptyList = []
+a = 1 : []
+b = 3 : (1 : [])
+c = [2,3,4] == 2 : 3 : 4 : []
+
+
+hailstoneSeq :: Integer -> [Integer]
+hailstoneSeq 1 = [1]
+hailstoneSeq n = n : hailstoneSeq (hailstone n)
+
 main = do
   print (sumatorial 10)
   print (hailstone 3)
   print [foo (-3), foo 0, foo 1, foo 36, foo 38]
+  print (sumPair (21, 21))
+  print (hailstoneSeq 5)
 
